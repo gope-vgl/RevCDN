84f6179fbc3da08760df1acbd1f723eae86aed12
Gonzalo Perez <gperez@vgl.cl>
2025-12-04 13:05:00 -0300

    new

--- scripts/generate_report.py
+++
@@ -1,5 +0,0 @@
-from report.config import REPConfig
-
-try:
-    config = REPConfig.load(path="./config/usr/rep_builder.json")
-
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,5 @@
 import json
-from .exceptions import FailedToOpenFile
+from exceptions import FailedToOpenFile
 
 def json_load(path: str) -> dict[str, str]:
     try:
@@ -9,8 +9,3 @@ def json_load(path: str) -> dict[str, str]:
         raise FailedToOpenFile(path=path, message=file_error.strerror)
     except json.JSONDecodeError as decode_error:
         raise FailedToOpenFile(path=path, message=decode_error.msg)
-
-
-def dict_to_json(output_path: str, data: dict[str, dict]) -> None:
-    with open(output_path, 'w') as jsonfile:
-        json.dump(data, jsonfile)
